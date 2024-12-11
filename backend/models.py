eb658c56e02e3e86da4424f271179a019d6ef593
aiaccfin <aiaccfin@gmail.com>
2024-12-10 19:20:52 -0500

    update

--- backend/models.py
+++ backend/models.py
@@ -1,14 +1,11 @@
 from sqlalchemy import Column, Integer, String
-from database import Base
-from database import engine
+from database import Base, engine
 
 class User(Base):
     __tablename__ = "fs_users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    id              = Column(Integer, primary_key=True, index=True)
+    username        = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-
-# Create the database tables if they don't exist
 
 User.metadata.create_all(bind=engine)

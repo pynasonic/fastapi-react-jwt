20ff65d6b116000ea920355f4f845e131def6a8f
aiaccfin <aiaccfin@gmail.com>
2024-12-10 19:14:52 -0500

    update

---
+++ backend/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import dotenv
+CFG = dotenv.dotenv_values(".env")
+SQLALCHEMY_DATABASE_URL = CFG['PYNASONIC_PG']
+
+# The `connect_args` parameter is needed only for SQLite.
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
